d3dc181e9630eac6fd5f83ef173841f0c07ebfd5
Sachin <jackwilder92@hotmail.com>
2017-06-09 02:31:51 +0530

    Created callback.php code

--- callback.php
+++ callback.php
@@ -1,3 +1,16 @@
 <?php
-echo "Working"
+    session_start();
+    require 'autoload.php';
+    use Abraham\TwitterOAuth\TwitterOAuth;
+
+    if(isset($_REQUEST['oauth_verifier'],$_REQUEST['oauth_token']) && 
+    $_REQUEST['oauth_token'] == $_SESSION['oauth_token']) {
+        $request_token = [];
+        $request_token['oauth_token'] = $_SESSION['oauth_token'];
+        $request_token['oauth_token_secret'] = $_SESSION['oauth_token_secret'];
+        $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token['oauth_token'],$request_token['oauth_token_secret']);
+        $access_token = $connection->oauth("oauth/access_token",array("oauth_verifier" => $_REQUEST['oauth_verifier']));
+        $_SESSION['access_token'] = $access_token;
+        header('Location: ./');
+    }
 ?>
