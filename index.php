fc1e59ded32d9877c51c22a97d2de90805578da8
Sachin <jackwilder92@hotmail.com>
2017-06-09 02:30:53 +0530

    Solved error in typos :wrench:

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if(!isset($_SESSION['access_token'])) {
     $url = $connection->url('oauth/authorize',array('oauth_token' => $request_token['oauth_token']));
     echo $url;
 } else {
-    $access_token = $_SESSION['acess_token'];
+    $access_token = $_SESSION['access_token'];
     $connection = new TwitterOAuth(CONSUMER_KEY,CONSUMER_SECRET,$access_token['oauth_token'],$access_token['oauth_token_secret']);
     $user = $connection -> get("account/verify_credentials");
     echo $user->status->text;
