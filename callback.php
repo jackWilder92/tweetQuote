2ec226437dc73392ae3b33881e1ea6a0130fdce8
Sachin <jackwilder92@hotmail.com>
2017-06-10 02:08:05 +0530

    Some fixes in urls

--- callback.php
+++ callback.php
@@ -11,6 +11,6 @@
         $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token['oauth_token'],$request_token['oauth_token_secret']);
         $access_token = $connection->oauth("oauth/access_token",array("oauth_verifier" => $_REQUEST['oauth_verifier']));
         $_SESSION['access_token'] = $access_token;
-        header('Location: ./');
+        header('Location: ./index.php');
     }
 ?>
--- index.php
+++ index.php
@@ -14,7 +14,8 @@ if(!isset($_SESSION['access_token'])) {
     $_SESSION['oauth_token'] = $request_token['oauth_token'];
     $_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
     $url = $connection->url('oauth/authorize',array('oauth_token' => $request_token['oauth_token']));
-    echo $url;
+    #echo $url;
+    header('Location: '. $url.'');
 } else {
     $access_token = $_SESSION['access_token'];
     $connection = new TwitterOAuth(CONSUMER_KEY,CONSUMER_SECRET,$access_token['oauth_token'],$access_token['oauth_token_secret']);
@@ -25,4 +26,5 @@ if(!isset($_SESSION['access_token'])) {
     $mediaUp = $connection -> upload('media/upload',['media' => './pictures/'.$random.'.jpg']);
     #print_r($mediaUp);
     $tweet = $connection -> post('statuses/update',['media_ids' => $mediaUp->media_id, 'status' => 'Image Tweeting']);
+    echo "Image posted.....";
 }
