09a333074731ff19f6c80d3117bde0854b3be684
Sachin <jackwilder92@hotmail.com>
2017-06-11 16:56:23 +0530

    New functionality for unique image post

--- index.php
+++ index.php
@@ -4,13 +4,9 @@ require 'autoload.php';
 include_once 'configs.php';
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-//First visit
-if (!file_exists('./pictures2')) {
-        mkdir('./pictures2', 0777, true);
-}
-
 //Steps to authorise app and get user oauth_token and oauth_token_secret
 if(!isset($_SESSION['access_token'])) {
+    delete_files('./pictures2');
     $connection = new TwitterOAuth(CONSUMER_KEY,CONSUMER_SECRET);
     $request_token = $connection->oauth('oauth/request_token',array('oauth_callback'=> OAUTH_CALLBACK));
     $_SESSION['oauth_token'] = $request_token['oauth_token'];
@@ -21,16 +17,42 @@ if(!isset($_SESSION['access_token'])) {
 } 
 //After authorization media tweet upload 
 else {
+    if (!file_exists('./pictures2')) {
+        mkdir('./pictures2', 0777, true);
+    }
     $access_token = $_SESSION['access_token'];
     $connection = new TwitterOAuth(CONSUMER_KEY,CONSUMER_SECRET,$access_token['oauth_token'],$access_token['oauth_token_secret']);
     $user = $connection -> get("account/verify_credentials");
     
-    $random = rand(1,8);
-    #$post = $connection -> post('statuses/update',array('status' => 'tweet it'));
-    $media = $random.'.jpg';
+    $flag = true;
+    while ($flag) {
+        $random = rand(1,9);
+        $media = $random.'.jpg';
+        if (!file_exists('./pictures2/'.$media)) {
+            $flag = false;
+        }
+        else {
+            echo "Tweet exist ".$media."\n";
+        }
+    }
     $mediaUp = $connection -> upload('media/upload',['media' =>'./pictures/'.$media]);
-    #print_r($mediaUp);
     $tweet = $connection -> post('statuses/update',['media_ids' => $mediaUp->media_id, 'status' => 'Image Tweeting']);
+    copy('./pictures/'.$media,'./pictures2/'.$media);
     echo '<img src = "./pictures/'.$media.'"alt="'.$media.'"/>';
     echo "Image posted.....";
+}
+
+function delete_files($target) {
+    if(is_dir($target)){
+        $files = glob( $target . '*', GLOB_MARK ); //GLOB_MARK adds a slash to directories returned
+        
+        foreach( $files as $file )
+        {
+            delete_files( $file );      
+        }
+      
+        rmdir( $target );
+    } elseif(is_file($target)) {
+        unlink( $target );  
+    }
 }
