46104d56460f59388716ef1116c4a74f4ae9a211
Sachin <jackwilder92@hotmail.com>
2017-06-11 15:20:17 +0530

    Made some fixes and added new functionality
    Working on unique image post

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
 session_start();
 require 'autoload.php';
+include_once 'configs.php';
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-define('CONSUMER_KEY','n7VHAXUcnfjtw3XAdTCXZps18');
-define('CONSUMER_SECRET','B78SOLezqtFLGdYqn8SmsEMflzkcLIO8ewWMfKyZWO2mSoSVuo');
-define('OAUTH_CALLBACK','http://localhost/tweetQuote/callback.php');
-
 //First visit
+if (!file_exists('./pictures2')) {
+        mkdir('./pictures2', 0777, true);
+}
+
+//Steps to authorise app and get user oauth_token and oauth_token_secret
 if(!isset($_SESSION['access_token'])) {
     $connection = new TwitterOAuth(CONSUMER_KEY,CONSUMER_SECRET);
     $request_token = $connection->oauth('oauth/request_token',array('oauth_callback'=> OAUTH_CALLBACK));
@@ -16,15 +18,19 @@ if(!isset($_SESSION['access_token'])) {
     $url = $connection->url('oauth/authorize',array('oauth_token' => $request_token['oauth_token']));
     #echo $url;
     header('Location: '. $url.'');
-} else {
+} 
+//After authorization media tweet upload 
+else {
     $access_token = $_SESSION['access_token'];
     $connection = new TwitterOAuth(CONSUMER_KEY,CONSUMER_SECRET,$access_token['oauth_token'],$access_token['oauth_token_secret']);
     $user = $connection -> get("account/verify_credentials");
     
     $random = rand(1,8);
     #$post = $connection -> post('statuses/update',array('status' => 'tweet it'));
-    $mediaUp = $connection -> upload('media/upload',['media' => './pictures/'.$random.'.jpg']);
+    $media = $random.'.jpg';
+    $mediaUp = $connection -> upload('media/upload',['media' =>'./pictures/'.$media]);
     #print_r($mediaUp);
     $tweet = $connection -> post('statuses/update',['media_ids' => $mediaUp->media_id, 'status' => 'Image Tweeting']);
+    echo '<img src = "./pictures/'.$media.'"alt="'.$media.'"/>';
     echo "Image posted.....";
 }
