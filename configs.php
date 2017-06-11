e41193baa234ccfb5fa2928d3ea138b6f2bb4f13
Sachin <jackwilder92@hotmail.com>
2017-06-11 15:19:16 +0530

    Added new configs.php
    New configuration file

---
+++ configs.php
@@ -0,0 +1,5 @@
+<?php
+define('CONSUMER_KEY','n7VHAXUcnfjtw3XAdTCXZps18');
+define('CONSUMER_SECRET','B78SOLezqtFLGdYqn8SmsEMflzkcLIO8ewWMfKyZWO2mSoSVuo');
+define('OAUTH_CALLBACK','http://localhost/tweetQuote/callback.php');
+?>
